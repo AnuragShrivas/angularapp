352c8243eb05d6a2a5951b05fe33ad20914cad82
AnuragShrivas <Anurag.a.shrivas@gmail.com>
2023-07-31 18:37:11 +0530

    FirstCommit

---
+++ angularapp/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { DemoComponent } from './demo/demo.component';
+import { Demo2Component } from './demo2/demo2.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    DemoComponent,
+    Demo2Component
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
